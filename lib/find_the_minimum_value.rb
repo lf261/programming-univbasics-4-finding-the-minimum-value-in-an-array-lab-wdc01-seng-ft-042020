f001e725557264c26369210d1fbef4bedaf8bf11
Luis Fernandez <luis.fernandez2093@gmail.com>
2020-04-06 19:58:31 +0000

    Automatically backed up by Learn

--- lib/find_the_minimum_value.rb
+++ lib/find_the_minimum_value.rb
@@ -3,8 +3,8 @@ def find_min_value(array)
   min_value = -1 
   
   while count < array.length do 
-    if min_value > array[0] 
-      min_value = array[0] 
+    if min_value > array[count] 
+      min_value = array[count] 
     end 
     count += 1 
   end 
